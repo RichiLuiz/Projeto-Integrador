a6c15bc2e020c37cf0d8fa549b5cb2bcd6a22369
Ricardo Luiz <161338480+RichiLuiz@users.noreply.github.com>
2026-09-14 21:04:36 +0000

    commit

--- api/db.js
+++ api/db.js
@@ -6,7 +6,9 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    family: 4
+    
 });
 
 async function connectDB() {
