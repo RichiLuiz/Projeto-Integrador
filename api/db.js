e6d805ddcfaa030c83132ffc1918aa2c3965be98
Ricardo Luiz <161338480+RichiLuiz@users.noreply.github.com>
2026-09-14 20:36:04 +0000

    commit db.js

--- api/db.js
+++ api/db.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,10 +12,13 @@ const pool = new Pool({
 async function connectDB() {
     try {
         const client = await pool.connect();
+
         console.log('Conectado ao PostgreSQL/Supabase');
+
         client.release();
     } catch (err) {
-        console.error('Erro ao conectar ao PostgreSQL/Supabase:', err);
+        console.error('Erro ao conectar ao PostgreSQL/Supabase:');
+        console.error(err);
     }
 }
 
